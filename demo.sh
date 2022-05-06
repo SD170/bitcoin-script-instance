36c6235b88f528e39fffe867bbc7f86d34ee369d
SD170 <duttasaswata7@gmail.com>
2022-05-06 23:51:15 +0530

    demo script

---
+++ demo.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+btcdeb '[OP_2 OP_1 OP_ADD]'
+# To follow the workings of the script, Keep on doing 
+#       $btcdeb> step 
+# in the terminal until the script-stack becomes empty,
+# or the execution stack has 0/1 as the last item.
+#
+# 1:successful/0:unsuccessful
