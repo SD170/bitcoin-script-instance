a800831014ab4b4f27162bae103156b3885ee1f3
SD170 <duttasaswata7@gmail.com>
2022-05-07 18:02:58 +0530

    pay-to-pub-key-hash added

---
+++ P2PKH.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# TXN URL: https://www.blockchain.com/btc/tx/5830ee28e2a4a46dee1450800a40ac307737ac274e0b7b7907c748d0917398c3
+
+btcdeb '[0014a64cdccb246b3c16e4261d0e05b55c77f1a5592b OP_HASH160 e638d12acdafcd13636e107d248f5f6325fcc5d2 OP_EQUAL]'
+# To follow the workings of the script, Keep on doing
+#       $btcdeb> step
+# in the terminal until the script-stack becomes empty,
+# or the execution stack has 0/1 as the last item.
+#
+# 1:successful/0:unsuccessful
